1a8c335783eecd3e3e47502ff2905f0033739b6c
yashasvi21aug-debug <yashasvi21aug@gmail.com>
2026-04-20 11:26:51 +0530

    simple calculator function

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-function checkNumber() {
-
-  let num = parseInt(document.getElementById("numberInput").value);
-
-  // Validation
-  if (isNaN(num)) {
-    alert("Please enter a valid number!");
-    return;
-  }
-
-  // Even or Odd
-  let resultText;
-  if (num % 2 === 0) {
-    resultText = "Even Number";
-  } else {
-    resultText = "Odd Number";
-  }
-
-  document.getElementById("result").innerHTML = resultText;
-
-  // Loop from 1 to num
-  let output = "<h5>Numbers from 1 to " + num + ":</h5>";
-
-  for (let i = 1; i <= num; i++) {
-    output += i + " ";
-  }
-
-  document.getElementById("numbers").innerHTML = output;
+// calculator.js
+function calculator(a, b, operator) {
+    if (operator === "+") return a + b;
+    if (operator === "-") return a - b;
+    if (operator === "*") return a * b;
+    if (operator === "/") return a / b;
 }
+
+console.log(calculator(10, 5, "+"));
+console.log(calculator(10, 5, "*"));
