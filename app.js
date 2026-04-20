efeca586aed74cac5989ea6144b65b19726f05ea
yashasvi21aug-debug <yashasvi21aug@gmail.com>
2026-04-20 11:18:16 +0530

    JS function

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-console.log("Hello from JavaScript!");
-function showAlert() {
-    alert("Hello cse 22");
-}
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+
+let arrayName = [22, "string", true, null];
+console.log(arrayName[0]);
+console.log(arrayName[1]);
+console.log(arrayName[2]);
+console.log(arrayName[3]);
+
+let div=function(a,b){
+    return a/b;
+};
+console.log(div(10,2));
+let greet = function() {
+  console.log("Hello");
+};
+greet();
+
+let greet2 = (name) => {
+  console.log("Hello, " + name + "!");
+};
+greet2("Yashasvi");
+
+let greet3 = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+greet3("karan");
